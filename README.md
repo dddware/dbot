0af8c4ef641b39594864dead58444f1d10d11fb8
neemzy <tom.panier@free.fr>
2014-01-25 16:27:37 +0100

    some refactoring, set up dpaste service, moved dfill response to a PM, updated README

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 dbot
 ====
 
-IRC bot
+**dbot** is an IRC bot designed (yet) to provide an interface to [dpaste](http://www.paste.cc) and [dfill](http://www.dfill.cc), respectively a code paster and a filler text generator.
+
+## Setup
+
+```shell
+npm install # install dependencies
+npm start # run bot
+```
+
+As for now, the server, channel list and nickname used by the bot are hardcoded. Separate config file coming soon!
+
+## Usage
+
+```irc
+!dpaste My content # creates a paste and sends you a PM with the URL
+!dfill n # sends you a PM with 1-100 lines of dummy text
+```
--- dbot.js
+++ dbot.js
@@ -1,71 +1,115 @@
 var irc = require('irc'),
-    http = require('follow-redirects').http;
+    http = require('follow-redirects').http,
+    qs = require('qs'),
 
+    dbot = {
+      client: null,
 
+      // Bot config
+      // TODO: read this from a config file on init
+      config: {
+        server: 'irc.freenode.net',
+        channels: ['#ddd'],
+        nick: 'dbot'
+      },
 
-var dbot = {
-  client: null,
+      // HTTP request parameters factories
+      http: {
+        dfill: function (amount) {
+          return {
+            host: 'www.dfill.cc',
+            port: 80,
+            method: 'GET',
+            path: '/api/' + amount
+          }
+        },
 
-  config: {
-    server: 'irc.freenode.net',
-    channels: ['#ddd'],
-    nick: 'dbot',
+        dpaste: function (length) {
+          return {
+            host: 'www.dpaste.cc',
+            port: 80,
+            method: 'POST',
+            path: '/paste/new',
+            headers: {
+              'User-Agent': 'curl',
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Content-Length': length
+            }
+          }
+        }    
+      },
 
-    dfill: function (amount) {
-      return {
-        host: 'dfill.cc',
-        port: 80,
-        method: 'GET',
-        path: '/api/' + amount
-      }
-    }
-  },
+      // Message matching handler
+      match: function (message, regex, callback) {
+        var matches = message.match(regex);
 
-  init: function () {
-    var bot = this;
-
-    bot.client = new irc.Client(
-      bot.config.server,
-      bot.config.nick,
-      {
-        channels: bot.config.channels
-      }
-    );
-
-    bot.client.addListener('error', function (message) {
-        console.log('error: ', message);
-    });
-
-    bot.client.addListener('message', function (from, to, text, message) {
-      // That's a channel message, right ?
-      if (to.indexOf('#') === 0) {
-        // dfill
-
-        var dfill_matches = text.match(/!dfill ([1-9][0-9]*)/);
-
-        if (dfill_matches) {
-          http.request(bot.config.dfill(dfill_matches[1]), function (res) {
-            res.setEncoding('utf8');
-            var buffer = '';
-
-            res.on('data', function (chunk) {
-              buffer += chunk;
-            });
-
-            res.on('end', function () {
-              bot.client.say(to, JSON.parse(buffer).join(' '));
-            });
-          }).end();
+        if (matches) {
+          callback(matches);
         }
+      },
 
+      // Main method
+      init: function () {
+        var bot = this;
 
+        bot.client = new irc.Client(
+          bot.config.server,
+          bot.config.nick,
+          {
+            channels: bot.config.channels
+          }
+        );
 
-        // More to come !
+        bot.client.addListener('error', function (message) {
+            console.log('error: ', message);
+        });
+
+        // Listen to channel messages
+        bot.client.addListener('message', function (from, to, message) {
+          if (to.indexOf('#') === 0) {
+
+            // dfill
+            bot.match(message, /!dfill ([1-9][0-9]*)/, function (matches) {
+              http.request(bot.http.dfill(matches[1]), function (res) {
+                res.setEncoding('utf8');
+                var buffer = '';
+
+                res.on('data', function (chunk) {
+                  buffer += chunk;
+                });
+
+                res.on('end', function () {
+                  bot.client.say(from, JSON.parse(buffer).join(' '));
+                });
+              }).end();
+            });
+
+            // dpaste
+            bot.match(message, /!dpaste (.+)/, function (matches) {
+              var data = qs.stringify({
+                    paste: matches[1]
+                  }),
+
+                  req = http.request(bot.http.dpaste(data.length), function (res) {
+                    res.setEncoding('utf8');
+                    var buffer = '';
+
+                    res.on('data', function (chunk) {
+                      buffer += chunk;
+                    });
+
+                    res.on('end', function () {
+                      bot.client.say(from, buffer);
+                    });
+                  });
+
+              req.write(data);
+              req.end();
+            });
+
+          }
+        });
       }
-    });
-  }
-};
-
-
+    };
 
 dbot.init();
